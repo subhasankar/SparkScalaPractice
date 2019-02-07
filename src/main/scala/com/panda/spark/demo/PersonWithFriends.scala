5969ce136fec2b1098bf031dcde7c90ede788519
subhasankar <subhasankar@outlook.com>
2019-02-07 15:17:44 +0530

    adding complex data fix

--- src/main/scala/com/panda/spark/demo/PersonWithFriends.scala
+++ src/main/scala/com/panda/spark/demo/PersonWithFriends.scala
@@ -1,13 +1,16 @@
 package com.panda.spark.demo
 
+import java.time.{LocalDate, ZoneId}
+import java.util.Date
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.types._
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 case class PersonInput(id:Int,name:String,age:Int,friends:String)
-case class Person(name:String,age:Int,dateM:String)
-case class PersonFriend(id:Int,name:String,age:Int,person:Person)
+//case class Person(name:String,age:Int,dateM:String)
+case class PersonFriend(id:Int,name:String,age:Int,friendName:String,fAge:Int,fdate:Long)
 
 object PersonWithFriends {
   def main(args: Array[String]): Unit = {
@@ -15,15 +18,17 @@ object PersonWithFriends {
     import spark.implicits._
     val personSchema=Encoders.product[PersonInput].schema
     val persons=spark.read.format("csv").option("delimiter","|").schema(personSchema).load("inputs/Person.dat")
-    persons.flatMap(a=>{
+    val mappedPerson=persons.flatMap(a=>{
       var l:ArrayBuffer[PersonFriend]=ArrayBuffer()
       val allF=a.getString(3).substring(2,a.getString(3).length-2).split("\\),\\(")
       allF.foreach(d=>{
         val f=d.split(",")
-        l+=PersonFriend(a.getInt(0),a.getString(1),a.getInt(2),Person(f(0),f(1).toInt,f(2)))
+        val c=f(2).split("\\/")
+        l+=PersonFriend(a.getInt(0),a.getString(1),a.getInt(2),f(0),f(1).toInt,LocalDate.of(c(2).toInt,c(1).toInt,c(0).toInt).atStartOfDay(ZoneId.systemDefault).toEpochSecond())
       })
       Some(l.toArray)
-    }).flatMap(x=>x).show(false)
+    }).flatMap(x=>x)
+    mappedPerson.orderBy(asc("fdate"),desc("fAge")).show(false)
     //employees.show(false)
     //employees.printSchema()
   }
--- src/main/scala/com/panda/spark/demo/entities/Test.java
+++ src/main/scala/com/panda/spark/demo/entities/Test.java
@@ -1,7 +1,13 @@
 package com.panda.spark.demo.entities;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 public class Test {
     public static void main(String[] args) {
+        new Date(2018,01,8);
+        System.out.println(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
         Integer [] ints={3,2,7,46,65,76,3,1,34,1};
         int min=Integer.MAX_VALUE;
         int max=0;
